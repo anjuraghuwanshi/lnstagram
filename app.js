43359da0e0135474f06a2eb4be41e579c233ecab
Anju <anjuraghuwanshi1601@gmail.com>
2024-06-04 19:15:14 +0530

    add https

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
 const express = require('express');
 const app = express();
-const {mongoUrl}= require('./keys');
-const port = process.env.port || 5000;
+const { mongoUrl } = require('./keys');
+const port = process.env.PORT || 5000; // Ensure PORT is uppercase
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-app.use(cors())
-require('./models/model')
-require('./models/post')
-app.use(express.json())
-app.use(require("./routes/auth"))
-app.use(require("./routes/createpost"))
-app.use(require("./routes/user"))
-mongoose.connect(mongoUrl)
 
-mongoose.connection.on('connected',()=>{
-    console.log("Connection established")
-})
-mongoose.connection.on('error',()=>{
-    console.log("Not connected")
-})
+// Middleware to redirect HTTP to HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  next();
+});
 
-//serving the frontend
-app.use(express.static(path.join(__dirname, "./frontend/build")))
-app.get("*",(req,res)=>{
-    res.sendFile(
-        path.join(__dirname,"./frontend/build/index.html"),
-        function(err){
-            res.status(500).send(err)
-        }
-    )
-})
+app.use(cors());
+require('./models/model');
+require('./models/post');
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/createpost'));
+app.use(require('./routes/user'));
 
-app.listen(port,()=>{
-    console.log("Server is running on port "+port);
-})
+mongoose.connect(mongoUrl);
 
+mongoose.connection.on('connected', () => {
+  console.log("Connection established");
+});
 
+mongoose.connection.on('error', () => {
+  console.log("Not connected");
+});
+
+// Serving the frontend
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
